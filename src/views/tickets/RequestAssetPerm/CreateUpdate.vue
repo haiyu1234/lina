4b6a6bed305651f615b083cfa026e21216f24c46
jxy <xiaoyu.jin@fit2cloud.com>
2025-01-06 15:58:09 +0800

    和谐爱奇技术定制化

--- src/views/tickets/RequestAssetPerm/CreateUpdate.vue
+++ src/views/tickets/RequestAssetPerm/CreateUpdate.vue
@@ -42,7 +42,7 @@ export default {
       fields: [
         [this.$t('common.Basic'), ['title', 'org_id']],
         [this.$t('tickets.RequestPerm'), [
-          'apply_nodes', 'apply_assets', 'apply_accounts',
+          'apply_assets', 'apply_accounts',
           'apply_actions', 'apply_date_start', 'apply_date_expired'
         ]],
         [this.$t('common.Other'), ['comment']]
@@ -75,8 +75,7 @@ export default {
               }
             },
             clearable: true
-          },
-          hidden: true
+          }
         },
         apply_assets: {
           type: 'assetSelect',
